bcad5c33e101d8d98a364195c7f9595ed0e0d4cc
Victor Holl <v_h@me.com>
2021-09-22 19:56:43 +0200

    setup

---
+++ lib/sellsy_v2.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "sellsy_v2/version"
+
+module SellsyV2
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "sellsy_v2"
+
+require "minitest/autorun"
